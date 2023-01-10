09b00a47ae8c5f59a80ad902caa0ae88ee57f2fd
Sergio Gomez <gomez.sergio14@gmail.com>
2023-01-09 21:16:51 -0300

    se cambió el componente card por item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,17 @@
 // import React from "react";
 
-function Item() {
+function Item(props) {
+    const {id,title,detail,imgurl}=props.item;
     const estilos={
         width:"18rem",
     }
   return (
-            <div className="card" style={estilos}>
+            <div className="card col-3 mx-2 my-3" style={estilos}>
+                <img src={imgurl} className="card-img-top" alt={id}></img>
                 <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{title}</h5>
                 <p className="card-text">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    {detail}
                 </p>  
                 </div>
             </div>
