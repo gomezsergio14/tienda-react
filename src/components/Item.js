5890cafc432eb451988cf0efbd8956cd6607b4d2
Sergio Gomez <gomez.sergio14@gmail.com>
2023-01-11 02:16:47 -0300

    boton ver mas agregado a item.js, todas las rutas configuradas,aplicacion funciona

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,12 @@
 // import React from "react";
+import { Link } from "react-router-dom";
 
 function Item(props) {
     const {id,title,detail,imgurl}=props.item;
     const estilos={
         width:"18rem",
     }
-  return (
+    return (
             <div className="card col-3 mx-2 my-3" style={estilos}>
                 <img src={imgurl} className="card-img-top" alt={id}></img>
                 <div className="card-body">
@@ -13,6 +14,7 @@ function Item(props) {
                 <p className="card-text">
                     {detail}
                 </p>  
+                <Link to={`detalle/${id}`} className="btn btn-primary">Ver más</Link>
                 </div>
             </div>
         );
