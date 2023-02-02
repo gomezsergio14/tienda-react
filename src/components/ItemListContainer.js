5eeabc458aa58f48f817f2fd4ef1017bfe59dbe2
Sergio Gomez <gomez.sergio14@gmail.com>
2023-02-02 14:18:49 -0300

    componente Loader creado

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,21 +2,27 @@ import { useState , useEffect } from "react";
 import Item from "./Item";
 import ObtenerProductos, { ObtenerProductoPorCategoria } from "../services/mockService";
 import { useParams } from "react-router-dom";
+import Loader from "./Loader";
 
 function ItemListContainer(props){
     const [productos,setProductos]=useState([]);
     let { nombreCategoria } = useParams();
+    const [isLoading,setIsLoading]=useState(true);
 
     useEffect(()=>{
         if(!nombreCategoria) { 
         ObtenerProductos()
             .then((respuesta)=>{
             setProductos(respuesta);
-        });}
+            setIsLoading(false);
+        })
+        .finally(()=>setIsLoading(false));}
         else {
             ObtenerProductoPorCategoria(nombreCategoria)
             .then((respuesta)=>{
-                setProductos(respuesta);});
+                setProductos(respuesta);
+                setIsLoading(false);})
+            .finally(()=>setIsLoading(false));
         }
     },[nombreCategoria]);
     
@@ -26,13 +32,19 @@ function ItemListContainer(props){
     return(
     <>
         <h1 style={miColor}>{props.text}</h1>
-        <div className="row d-flex justify-content-center">
-        {
-            productos.map((item)=>{return <Item item={item}/>})
-        }
+        {isLoading?
+            (
+            <Loader/>
+            )
+            :(
+            <div className="row d-flex justify-content-center">
+            {
+                productos.map((item)=>{return <Item item={item}/>})
+            }
              {/* <Item item={Item}/>  */}
-        
         </div>
+        )}
+        
     </>    
     )
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../App.css";
+
+function Loader() {
+    return (
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default Loader
